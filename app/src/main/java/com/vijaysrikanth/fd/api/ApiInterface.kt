ddb6662f8822223bdd696f3753eeaf349dc7d408
VIJAY <sabbe.vijayasrikanth@gmail.com>
2022-02-08 19:05:04 +0530

    vijay

--- app/src/main/java/com/vijaysrikanth/fd/api/ApiInterface.kt
+++ app/src/main/java/com/vijaysrikanth/fd/api/ApiInterface.kt
@@ -7,7 +7,6 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
-import retrofit2.http.Query
 
 interface ApiInterface {
 
